1a313405209c2a2c1f1d871f76b77dc76178fbd1
mahi143 <maheswari.panda@gmail.com>
2020-10-11 10:39:33 +0100

    First setup commit

---
+++ src/test/java/com/shopping/promotion/engine/ShoppingCartTest.java
@@ -0,0 +1,17 @@
+package com.shopping.promotion.engine;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for shopping cart.
+ */
+public class ShoppingCartTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
