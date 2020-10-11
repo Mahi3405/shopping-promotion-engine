8ce9e2c0e21cb105e70ab5a3c839d1eccee7c71e
mahi143 <maheswari.panda@gmail.com>
2020-10-11 11:48:25 +0100

    Promotion from Different SKUs

--- src/main/java/com/shopping/promotion/engine/ShoppingCart.java
+++ src/main/java/com/shopping/promotion/engine/ShoppingCart.java
@@ -1,10 +1,17 @@
 package com.shopping.promotion.engine;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.shopping.promotion.engine.entity.Item;
+import com.shopping.promotion.engine.util.Product;
+import com.shopping.promotion.engine.util.Promo;
 
 public class ShoppingCart {
+	
+	private final Predicate<Item> isBothSkuPromoApplicable = item -> (item.getSku() == Product.SKU_C || item.getSku() == Product.SKU_D);
 	private List<Item> items;
 
 	public ShoppingCart(List<Item> items) {
@@ -13,7 +20,34 @@ public class ShoppingCart {
 	}
 
 	public double getTotal() {
+		
+		if(isCartContainsBothSkus())
+		{
+			return getAmountForMultiSkus();
+		}
+		
 		return items.stream().mapToDouble(item -> item.getSubTotal(item)).sum();
 	}
+
+	public boolean isCartContainsBothSkus() {
+		List<Product> listWithBothSkus = items.stream().filter(isBothSkuPromoApplicable).map(item -> item.getSku()).collect(Collectors.toList());
+		
+		return listWithBothSkus.containsAll(Arrays.asList(new Product[] { Product.SKU_C, Product.SKU_D }));
+	}
+
+	public double getAmountForMultiSkus() {
+		int cQuantity = items.stream().filter(item -> item.getSku().equals(Product.SKU_C)).findFirst().get().getQunantity();
+		
+		int dQuantity = items.stream().filter(item -> item.getSku().equals(Product.SKU_D)).findFirst().get().getQunantity();
+		
+		int qtyPromo = cQuantity >= dQuantity ? dQuantity : cQuantity;
+
+		double extraQty = cQuantity >= dQuantity ? (cQuantity - dQuantity) * Product.SKU_C.getPrice()
+				: (dQuantity - cQuantity) * Product.SKU_D.getPrice();
+		
+		return qtyPromo * Promo.TYPE_C_D.getPromoPrice() + extraQty;
+	}
+
+	
 	
 }
--- src/test/java/com/shopping/promotion/engine/ShoppingCartTest.java
+++ src/test/java/com/shopping/promotion/engine/ShoppingCartTest.java
@@ -1,12 +1,15 @@
 package com.shopping.promotion.engine;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+
 import com.shopping.promotion.engine.entity.Item;
 import com.shopping.promotion.engine.util.Product;
 
@@ -42,6 +45,27 @@ public class ShoppingCartTest
 	{
     	ShoppingCart cart =buildShoppingCart(new Item(2, Product.SKU_A), new Item(2, Product.SKU_B));
     	assertEquals(160.0, cart.getTotal(), 0.0);
+	}
+   
+    @Test
+	public void test_isCartContainsBothCDSkus() {
+		ShoppingCart cart = buildShoppingCart(new Item(3, Product.SKU_A), new Item(5, Product.SKU_B),
+				new Item(1, Product.SKU_C), new Item(1, Product.SKU_D));
+		assertTrue(cart.isCartContainsBothSkus());
+	}
+    
+    @Test
+   	public void test_isCartContainsBothCDSkus_False() {
+   		ShoppingCart cart = buildShoppingCart(new Item(3, Product.SKU_A), new Item(5, Product.SKU_B),
+   				new Item(1, Product.SKU_C));
+   		assertFalse(cart.isCartContainsBothSkus());
+   	}
+    
+    @Test
+	public void test_getTotalOfMultiSkuPromotion() {
+		ShoppingCart cart = buildShoppingCart(new Item(3, Product.SKU_A), new Item(5, Product.SKU_B),
+				new Item(1, Product.SKU_C), new Item(2, Product.SKU_D));
+		assertEquals(45.0, cart.getTotal(), 0.0);
 	}
     
     private ShoppingCart buildShoppingCart(Item... items) {
